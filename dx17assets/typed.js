1eefa6a05b0ce1ceff3dca770abdf26ede08d676
dx171337 <1337dx17@proton.me>
2023-07-27 09:47:33 -0400

    first commit

---
+++ dx17assets/typed.js
@@ -0,0 +1,7 @@
+new Typed('#typed',{
+  strings : [,'dx17#1337','dx on top','add my roblox lol - AkameMepzu','i might just end up with ur identity','dx loves stacking',],
+  typeSpeed : 35,
+  delaySpeed : 90,
+  showCursor: false,
+  loop : true
+})
